6560e673b3cc53147bc77ecbac896d797758a010
Albie <albion@dragonfruit.network>
2022-07-12 16:40:27 +0100

    add updater component

---
+++ src/Updater/ISoftwareUpdater.cs
@@ -0,0 +1,29 @@
+﻿// DragonFruit Desktop Shared Components Copyright DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under MIT. Refer to the LICENSE file for more info
+
+using System;
+using System.Threading.Tasks;
+
+namespace DragonFruit.Software.Desktop.Updater
+{
+    /// <summary>
+    /// Represents a component responsible for processing application updates
+    /// </summary>
+    public interface ISoftwareUpdater
+    {
+        /// <summary>
+        /// Performs a check for updates, applying them if available
+        /// </summary>
+        /// <param name="progressChanged">
+        /// Callback used when the progress has changed.
+        /// Accepts a <see cref="string"/> for textual progress updates and an <see cref="int"/> for progress bar feedback
+        /// </param>
+        /// <param name="allowDeltaPatches">Whether to allow delta patches to be used.</param>
+        Task<bool> Perform(Action<string, int?> progressChanged, bool allowDeltaPatches = true);
+
+        /// <summary>
+        /// Schedules the client to be restarted, rebooting the latest version.
+        /// </summary>
+        void Restart();
+    }
+}
---
+++ src/Updater/SquirrelUpdater.cs
@@ -0,0 +1,74 @@
+﻿// DragonFruit Desktop Shared Components Copyright DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under MIT. Refer to the LICENSE file for more info
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Serilog;
+using Squirrel;
+
+namespace DragonFruit.Software.Desktop.Updater
+{
+    /// <summary>
+    /// Represents a <see cref="ISoftwareUpdater"/> using Squirrel as the backend
+    /// </summary>
+    public class SquirrelUpdater : ISoftwareUpdater, IDisposable
+    {
+        private readonly UpdateManager _manager;
+
+        public SquirrelUpdater(string appName, string url)
+        {
+            var isGitHub = url.Contains("github.com");
+            _manager = isGitHub ? new GithubUpdateManager(url, applicationIdOverride: appName) : new UpdateManager(url, appName);
+        }
+
+        /// <inheritdoc />
+        public async Task<bool> Perform(Action<string, int?> progressChanged, bool useDeltaPatching = true)
+        {
+            try
+            {
+                var info = await _manager.CheckForUpdate(!useDeltaPatching).ConfigureAwait(false);
+
+                if (!info.ReleasesToApply.Any())
+                {
+                    return false;
+                }
+
+                try
+                {
+                    await _manager.DownloadReleases(info.ReleasesToApply, p => progressChanged.Invoke($"Downloading Update ({p}% Complete)", p)).ConfigureAwait(false);
+                    await _manager.ApplyReleases(info, p => progressChanged.Invoke($"Installing ({p}% Complete)", p)).ConfigureAwait(false);
+
+                    progressChanged.Invoke("Update Complete.", 100);
+
+                    return true;
+                }
+                catch (Exception e) when (useDeltaPatching)
+                {
+                    Log.Warning("Error updating with deltas: {@Exception}", e);
+                    await Perform(progressChanged, false).ConfigureAwait(false);
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Error("Error Updating: {@Exception}", e);
+                progressChanged.Invoke("Update Failed.", null);
+
+                await Task.Delay(1250).ConfigureAwait(false);
+            }
+
+            return false;
+        }
+
+        /// <inheritdoc />
+        public void Restart()
+        {
+            UpdateManager.RestartApp();
+        }
+
+        public void Dispose()
+        {
+            _manager?.Dispose();
+        }
+    }
+}
