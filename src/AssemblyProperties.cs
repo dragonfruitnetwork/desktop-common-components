67e4c96485eebb0d4aa4343aab2eeba971e3b67f
Albie <albion@dragonfruit.network>
2022-07-12 16:40:17 +0100

    add global platform support attribute

---
+++ src/AssemblyProperties.cs
@@ -0,0 +1,6 @@
+﻿// DragonFruit Desktop Shared Components Copyright DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under MIT. Refer to the LICENSE file for more info
+
+using System.Runtime.Versioning;
+
+[assembly: SupportedOSPlatform("windows")]
